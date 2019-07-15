7ef9ad98bbaba94b449e0b454425d8b9b81ebdb3
lucagez <lucagesmundo@yahoo.it>
2019-07-15 09:26:11 +0200

    optimizing split

--- src/_makeChunks.mjs
+++ src/_makeChunks.mjs
@@ -1,21 +1,30 @@
+// export default (str, regex) => str
+//   .replace(regex, (type) => {
+//     switch (type) {
+//       // returning __par__ enclosed by ""
+//       // => When splitting there will be a " on each side.
+//       case '"string__sjs"':
+//       case '"undefined__sjs"':
+//         return '"__par__"';
+
+//       // When stringifying a function inside an array [null] is returned.
+//       // => Using [null] as an identifier for array schema.
+//       case '"number__sjs"':
+//       case '"boolean__sjs"':
+//       case '["array-simple__sjs"]':
+//       case '[null]':
+//         return '__par__';
+//       default:
+//         return type;
+//     }
+//   })
+//   .split('__par__');
+
 export default (str, regex) => str
   .replace(regex, (type) => {
-    switch (type) {
-      // returning __par__ enclosed by ""
-      // => When splitting there will be a " on each side.
-      case '"string__sjs"':
-      case '"undefined__sjs"':
-        return '"__par__"';
-
-      // When stringifying a function inside an array [null] is returned.
-      // => Using [null] as an identifier for array schema.
-      case '"number__sjs"':
-      case '"boolean__sjs"':
-      case '["array-simple__sjs"]':
-      case '[null]':
-        return '__par__';
-      default:
-        return type;
+    if (type === '"string__sjs"') {
+      return '"__par__"';
     }
+    return '__par__';
   })
   .split('__par__');
